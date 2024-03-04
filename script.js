32087339e40f73612bbb1fbe7bc4c0984e483873
arthur aro <arthurdearoramos@outlook.com>
2024-03-04 17:15:09 -0300

    readme.md

--- script.js
+++ script.js
@@ -40,3 +40,54 @@ document.addEventListener("DOMContentLoaded", function () {
   setActiveLink(menuLinks[0])
 
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+  const charactersSection = document.getElementById("characters")
+  const personagensLink = document.querySelector('a[href="#characters"]')
+
+  personagensLink.addEventListener("click", function (event) {
+    event.preventDefault()
+    charactersSection.classList.toggle("hidden");
+  })
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  // ...
+
+  const story1Link = document.querySelector('a[href="#story1"]')
+  const story1Complete = document.querySelector("#story1 .hidden")
+
+  story1Link.addEventListener("click", function (event) {
+    event.preventDefault()
+    story1Complete.classList.toggle("hidden")
+  })
+
+  const story2Link = document.querySelector('a[href="#story2"]')
+  const story2Complete = document.querySelector("#story2 .hidden")
+
+  story2Link.addEventListener("click", function (event) {
+    event.preventDefault()
+    story2Complete.classList.toggle("hidden")
+  })
+
+  const story3Link = document.querySelector('a[href="#story3"]')
+  const story3Complete = document.querySelector("#story3 .hidden")
+
+  story3Link.addEventListener("click", function (event) {
+    event.preventDefault()
+    story3Complete.classList.toggle("hidden")
+  })
+
+  const story4Link = document.querySelector('a[href="#story4"]')
+  const story4Complete = document.querySelector("#story4 .hidden")
+
+  story4Link.addEventListener("click", function (event) {
+    event.preventDefault()
+    story4Complete.classList.toggle("hidden")
+  })
+
+
+  // Repita o padrão para outras histórias, conforme necessário.
+})
+
+
