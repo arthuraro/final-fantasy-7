abcf2d939fefe997a30edc7efb49fba8760a61c8
arthur aro <arthurdearoramos@outlook.com>
2024-03-22 17:14:12 -0300

    readme.md

--- script.js
+++ script.js
@@ -32,6 +32,9 @@ document.addEventListener("DOMContentLoaded", function () {
     link.addEventListener("click", navigateToSection)
     link.dataset.index = index + 1
   })
+
+  showSection("home")
+  setActiveLink(menuLinks[0])
 })
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -45,39 +48,9 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 
 document.addEventListener("DOMContentLoaded", function () {
-  // ...
-
-  const story1Link = document.querySelector('a[href="#story1"]')
-  const story1Complete = document.querySelector("#story1 .hidden")
-
-  story1Link.addEventListener("click", function (event) {
-    event.preventDefault()
-    story1Complete.classList.toggle("hidden")
-  })
-
-  const story2Link = document.querySelector('a[href="#story2"]')
-  const story2Complete = document.querySelector("#story2 .hidden")
-
-  story2Link.addEventListener("click", function (event) {
-    event.preventDefault()
-    story2Complete.classList.toggle("hidden")
-  })
-
-  const story3Link = document.querySelector('a[href="#story3"]')
-  const story3Complete = document.querySelector("#story3 .hidden")
-
-  story3Link.addEventListener("click", function (event) {
-    event.preventDefault()
-    story3Complete.classList.toggle("hidden")
-  })
-
-  const story4Link = document.querySelector('a[href="#story4"]')
-  const story4Complete = document.querySelector("#story4 .hidden")
-
-  story4Link.addEventListener("click", function (event) {
-    event.preventDefault()
-    story4Complete.classList.toggle("hidden")
-  })
-
-  // Repita o padrão para outras histórias, conforme necessário.
+  const copyright = document.getElementById("copyright")
+  copyright.innerHTML =
+    "&copy; " +
+    new Date().getFullYear() +
+    " Square Enix Holdings Co., Ltd. Todos os direitos reservados."
 })
